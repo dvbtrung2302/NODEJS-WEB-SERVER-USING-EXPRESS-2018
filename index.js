1e2b238d945ca78de6931ea3901aa7749a28c1a9
Dang Vo Bao Trung <baotrung23022000@gmail.com>
2020-02-11 00:11:10 +0700

    Post method

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var port = 3000;
 var users = [
@@ -8,6 +9,9 @@ var users = [
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', function(req, res) {
 	res.render('index', {
@@ -31,6 +35,15 @@ app.get('/users/search', function(req, res) {
 		users: matchedUsers
 	});
 
+});
+
+app.get('/users/create', function(req, res) {
+	res.render('users/create');
+});
+
+app.post('/users/create', function(req, res) {
+	users.push(req.body);
+	res.redirect('/users');
 });
 
 app.listen(port, function() {
