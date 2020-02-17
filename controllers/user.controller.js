c91ce64892c8292c3ea6c59b7fd1d9237081b063
Dang Vo Bao Trung <baotrung23022000@gmail.com>
2020-02-17 23:11:45 +0700

    middleware

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -25,7 +25,6 @@ module.exports.create = function(req, res) {
 module.exports.get = function(req, res) {
 	var id = req.params.id;
 	var user = db.get('users').find({ id: id }).value();
-
 	res.render('users/view', {
 		user: user
 	});
@@ -33,21 +32,7 @@ module.exports.get = function(req, res) {
 
 module.exports.postCreate = function(req, res) {
 	req.body.id = shortid.generate();
-	errors 	= [];
-	if (!req.body.name) {
-		errors.push('Name is required.');
-	}
-	if (!req.body.phone) {
-		errors.push('Phone is required.');
-	}
-	if(errors.length) {
-		res.render('users/create', {
-			errors: errors,
-			values: req.body
-		});
-		return;
-	}
-
+	console.log(res.locals);
 	db.get('users').push(req.body).write();
 	res.redirect('/users');
 };
